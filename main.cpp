b1aa02238cd7ca188de9d9926928b0a2561ea0b3
DandeVamshiVardhan <vamshivamshi3452@gmail.com>
2025-05-28 01:51:22 +0530

    git_practicing

--- main.cpp
+++ main.cpp
@@ -16,12 +16,7 @@ int getFactorialSum(int n)
 {
     vector<int>vec(n+1,-1);
     vec[0]=0;
-    for(int i=1;i<=n;i++){
-        vec[i] = getFact(i,vec);
-    }
-    int ans=0;
-    cout<<endl;
-    return ans;
+
 }
 
 
